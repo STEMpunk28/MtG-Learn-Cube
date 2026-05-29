7b861f2c36c01d7a0a29fbead3873bb36276d9cd
Sebastian Saez <sebastian.saez@ug.uchile.cl>
2026-05-29 16:16:00 -0400

    Added Context to app

--- learn-app/src/main.tsx
+++ learn-app/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { SavedLessonsProvider } from "./context/SavedLessonsContext";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SavedLessonsProvider>
+      <App />
+    </SavedLessonsProvider>
   </StrictMode>,
 )
